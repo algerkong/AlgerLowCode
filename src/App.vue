ff36347f78dd99de5b92365a15ee13ed3d1a81c5
algerkc@qq.com <algerkc@qq.com>
2023-11-06 18:20:22 +0800

    🎉 init: 项目初始化

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <draggable v-model="myArray" group="people" item-key="id" @start="drag = true" @end="drag = false">
+    <template #item="{ element }">
+      <div class="item">{{ element.name }}</div>
+    </template>
+  </draggable>
+</template>
+
+<script lang="ts" setup>
+import { reactive, toRefs } from 'vue'
+import draggable from 'vuedraggable'
+
+const data = reactive({
+  drag: false,
+  myArray: [
+    { id: 1, name: 'Jenny1111' },
+    { id: 2, name: 'kevin' },
+    { id: 3, name: 'lili' },
+  ],
+})
+const { drag, myArray } = toRefs(data)
+</script>
+
+<style lang="scss" scoped>
+body {
+  @apply bg-black;
+}
+.item {
+  @apply bg-blue-600 p-2 m-10 rounded-lg shadow-xl shadow-gray-800 cursor-move text-gray-50;
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { TDesignResolver } from 'unplugin-vue-components/resolvers';
+import eslintPlugin from 'vite-plugin-eslint';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [
+		vue(),
+		AutoImport({
+			resolvers: [
+				TDesignResolver({
+					library: 'vue-next',
+				}),
+			],
+		}),
+		Components({
+			resolvers: [
+				TDesignResolver({
+					library: 'vue-next',
+				}),
+			],
+		}),
+		eslintPlugin({
+			include: ['src/**/*.ts', 'src/**/*.vue', 'src/*.ts', 'src/*.vue'],
+		}),
+	],
+	resolve: {
+		// 配置路径别名
+		alias: {
+			'@': '/src',
+		},
+	},
+});
