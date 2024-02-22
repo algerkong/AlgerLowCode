4e14f07d8b34000acff396aaa8b92c4b66922161
algerkc@qq.com <algerkc@qq.com>
2024-02-22 11:28:47 +0800

    ✨ feat: 修改样式

--- src/designer/elements/BaseElement.vue
+++ src/designer/elements/BaseElement.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="element" :class="{ 'element-select': selectIndex === index }" @click.stop="widgetList.selectWidget(index)">
     <component :is="Elements[element.type]" v-model="models[element.key]" :placeholder="element.description"></component>
-    <t-button theme="warning" @click="widgetList.deleteWidget(element)">删除</t-button>
-    <t-icon name="drag-move" class="draggable-item-move text-gray-400" />
+    <div class="draggable-item-move">
+      <div class="element-action-item">
+        <t-icon name="drag-move" />
+      </div>
+    </div>
+    <div class="element-action">
+      <div class="element-action-item" @click="widgetList.deleteWidget(element)">
+        <t-icon name="delete" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,9 +27,22 @@ const props = defineProps<{
 
 <style scoped lang="scss">
 .element {
-  @apply p-2 mb-2 transition-all border-2 border-transparent;
+  @apply p-2 mb-2 transition-all border-2 border-transparent relative;
 }
 .element-select {
-  @apply border-blue-500;
+  @apply bg-slate-200 rounded-sm;
+  .draggable-item-move,
+  .element-action {
+    @apply block;
+  }
+}
+.draggable-item-move {
+  @apply absolute left-0 bottom-0 cursor-move hidden z-50;
+}
+.element-action {
+  @apply absolute right-0 bottom-0 hidden cursor-pointer  z-50;
+}
+.element-action-item {
+  @apply flex items-center justify-center bg-gray-400 px-1 text-xs text-white h-5;
 }
 </style>
